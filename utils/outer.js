ed07515f915251455320a2c24582f286f7cb2b8c
Anitus A <anitus.cs24@stellamaryscoe.edu.in>
2025-10-08 23:56:02 +0530

    fix typo in description field in outer.js

--- utils/outer.js
+++ utils/outer.js
@@ -9,7 +9,7 @@ const outer = {
         'I build things for the web',
         'A Traveler',
     ],
-    desciption: `A dedicated and disciplined Computer Science and Engineering student who loves to create things for the internet. Having more than 4 years of field experience, I've delivered projects to many happy clients across the globe.`,
+    description: `A dedicated and disciplined Computer Science and Engineering student who loves to create things for the internet. I've delivered projects to many happy clients across the globe.`,
     button: {
         label: 'Contact me!',
         onClick: () => openLink('mailto:anitus2006ajr@gmail.com?subject=Hello')
