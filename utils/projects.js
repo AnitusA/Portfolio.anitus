e61010a15e9bf3d8b7a3acb77cd33b0c6bd97200
Anitus A <anitus.cs24@stellamaryscoe.edu.in>
2026-01-20 23:32:45 +0530

    Updation

--- utils/projects.js
+++ utils/projects.js
@@ -2,7 +2,7 @@ const projects = {
     heading: `Things I've Worked on, Some of Them`,
     list: [
         {
-            image: '/assets/projects/discord-bot.png',
+            image: '/assets/project1.jpg',
             label: 'Automation',
             title: 'Discord Bot',
             description: 'Designed and deployed a custom Discord bot to automate server interactions and enhance community engagement. This bot handles tasks like welcoming new members, moderating content, and responding to user commands with real-time data. Built using JavaScript and Discord.js, it features modular command handling, role assignment, and API integration for dynamic responses.',
@@ -13,7 +13,7 @@ const projects = {
             ]
         },
         {
-            image: '/assets/projects/cms.png',
+            image: '/assets/project2.jpg',
             label: 'Featured Project',
             title: 'CMS - Classroom Management System',
             description: 'A comprehensive platform for managing classroom activities, assignments, and student interactions. This system streamlines communication between teachers and students, facilitates assignment submissions, and provides a centralized hub for educational resources. Built with a focus on user experience and accessibility.',
